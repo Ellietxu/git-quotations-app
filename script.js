65e674bb1681372c94e6b9f60d422ed960a32e17
Elodie S <elodie.sevestre86@gmail.com>
2026-03-05 15:55:18 +0100

    steps 0-3 done

---
+++ script.js
@@ -0,0 +1,9 @@
+//! Declaration des constantes
+
+const citationForm = document.getElementById("citationForm");
+
+// on écoute le submit du form
+citationForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // empêche le rechargement de la page
+  // traitement du formulaire en JS
+});
