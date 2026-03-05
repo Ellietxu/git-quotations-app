c8d68dbe6f426f2935e0d5d6162746af567f405b
Elodie S <elodie.sevestre86@gmail.com>
2026-03-05 17:00:06 +0100

    step 4-5 done

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //! Declaration des constantes
 
 const citationForm = document.getElementById("citationForm");
+const citationInput = document.getElementById("citationInput");
+const authorInput = document.getElementById("authorInput");
+const quoteList = document.getElementById("quote-list");
 
 // on écoute le submit du form
 citationForm.addEventListener("submit", function (event) {
   event.preventDefault(); // empêche le rechargement de la page
-  // traitement du formulaire en JS
+  //* on recupere le contenu des inputs et on les stock dans des variables
+  const textForm = citationInput.value;
+  const authorForm = authorInput.value;
+  addQuote(textForm, authorForm);
+  citationInput.value = "";
+  authorInput.value = "";
 });
+
+function addQuote(quote, author) {
+  const quoteAdd = document.createElement("p");
+  quoteAdd.classList.add("text");
+  quoteAdd.textContent = `Citation : "${quote}"`;
+
+  const authorAdd = document.createElement("p");
+  authorAdd.classList.add("author");
+  authorAdd.textContent = `Autrice/auteur : ${author}`;
+
+  const divQuote = document.createElement("div");
+  divQuote.classList.add("quote");
+  quoteList.appendChild(divQuote);
+  divQuote.appendChild(quoteAdd);
+  divQuote.appendChild(authorAdd);
+}
+
+//     mettre le texte de quote dans le paragraphe citation
+//     mettre le texte de author dans le paragraphe auteur
+
+// Fonctionnalités JS à utiliser :
+
+// créer un élément → document.createElement()
+
+// ajouter une classe → element.classList.add()
+
+// ajouter du texte → element.textContent
