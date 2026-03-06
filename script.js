06edd86f204717036bca0f740457561a4533609b
Elodie S <elodie.sevestre86@gmail.com>
2026-03-06 14:36:33 +0100

    begin step 7

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ const citationForm = document.getElementById("citationForm");
 const citationInput = document.getElementById("citationInput");
 const authorInput = document.getElementById("authorInput");
 const quoteList = document.getElementById("quote-list");
+let quoteCount = 0;
 
-// on écoute le submit du form
-citationForm.addEventListener("submit", function (event) {
+//! déclaration JSON dans script.js
+
+//* objet json : const x = {clé1 : valeur, clé2: valeur...}
+//* ajout [] autour des {} du json pour transformer en tableau et ainsi ajouter d'autres objets
+const data = [
+  {
+    author: "Simone de Beauvoir",
+    text: "On ne naît pas femme, on le devient.",
+  },
+];
+// console.log(data[0].author); // accéder au clés grâce à l'index
+
+//! on écoute le submit du form
+
+citationForm.addEventListener("submit", (event) => {
+  // ici fonction fléchée
   event.preventDefault(); // empêche le rechargement de la page
   //* on recupere le contenu des inputs et on les stock dans des variables
   const textForm = citationInput.value;
   const authorForm = authorInput.value;
   addQuote(textForm, authorForm);
-  citationInput.value = "";
-  authorInput.value = "";
 });
 
 function addQuote(quote, author) {
   const quoteAdd = document.createElement("p");
-  quoteAdd.classList.add("text");
+  quoteAdd.classList.add("text"); // possibilité = quoteAdd.className = "text"
   quoteAdd.textContent = `🗨️ "${quote}"`;
 
   const authorAdd = document.createElement("p");
@@ -30,15 +43,27 @@ function addQuote(quote, author) {
   quoteList.appendChild(divQuote);
   divQuote.appendChild(quoteAdd);
   divQuote.appendChild(authorAdd);
+
+  quoteCount += 1;
+  const count = document.getElementById("count");
+  count.innerText = `${quoteCount} citations`;
 }
 
-//     mettre le texte de quote dans le paragraphe citation
-//     mettre le texte de author dans le paragraphe auteur
+//! vérifier si il y a quelque chose dans localStorage
 
-// Fonctionnalités JS à utiliser :
+//* récupérer le tableau depuis localStorage
+const storedData = localStorage.getItem("citations");
 
-// créer un élément → document.createElement()
+//* vérifier son existence (possibilité d'utiliser try/cash)
 
-// ajouter une classe → element.classList.add()
+if (storedData) {
+  citations = JSON.parse(storedData);
+} else {
+  citations = [];
+}
 
-// ajouter du texte → element.textContent
+//* sauvegarder nouvelle citation dans localStorage
+const storedCitation = localStorage.setItem("citations", JSON.stringify(data));
+console.log(storedCitation);
+
+//*
